16fd82837c71957d3186354f29c66931341571ee
Arela-Cristina <instagrandine@gmail.com>
2024-12-06 16:39:27 +0100

    create Home Page and add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./assets/Components/Body/Home-Page.jsx"
 import Nav from "./assets/Components/Menu-Nav/Nav.jsx"
 import About from "./assets/Components/Body/About-Us.jsx"
 import Footer from "./assets/Components/Footer/Footer.jsx"
@@ -10,9 +11,13 @@ function App() {
     <>
       <BrowserRouter>
         <Nav />
-        <About />
-        <Footer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Footer" element={<Footer />} />
+        </Routes >
       </BrowserRouter>
+
 
     </>
   )
