4b6928324d75c8e033cf3ec4ef189bb83a131854
Arela-Cristina <instagrandine@gmail.com>
2024-12-06 16:11:35 +0100

    add Menu structure and style

--- src/assets/Components/Menu-Nav/Menu-Nav.jsx
+++ src/assets/Components/Menu-Nav/Menu-Nav.jsx
@@ -1,7 +1,9 @@
+import style from "./Menu-Nav.module.css";
+
 export default function  Nav(){
     return (
         <>
-         <ul>
+         <ul className={style.menu}>
             <li><a href="">Home</a></li>
             <li><a href="">About Us</a></li>
             <li><a href="">Posts List</a></li>
