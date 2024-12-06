450e88e062952cd24d64c299930862a16d63da84
Arela-Cristina <instagrandine@gmail.com>
2024-12-06 18:40:02 +0100

    add style

--- src/assets/Components/Menu-Nav/Nav.jsx
+++ src/assets/Components/Menu-Nav/Nav.jsx
@@ -2,13 +2,16 @@ import style from "./Nav.module.css"
 import { Link, NavLink } from "react-router-dom"
 
 export default function Nav() {
+
+  const handlerClassName = ({ isActive }) => isActive ? style.active : ''
+
   return (
     <>
       <nav>
         <ul className={style.menu}>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/About">About Us</NavLink>
-          <NavLink to="/Posts">Posts</NavLink>
+          <NavLink className={handlerClassName} to="/">Home</NavLink>
+          <NavLink className={handlerClassName} to="/About">About Us</NavLink>
+          <NavLink className={handlerClassName} to="/Posts">Posts</NavLink>
         </ul>
       </nav>
     </>
